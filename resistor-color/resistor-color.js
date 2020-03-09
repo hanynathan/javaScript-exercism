81f2742e7db40604aa675467ada2db4c90aa38e3
Hany <hanynathan@yahoo.com>
2020-03-09 10:49:01 +0200

    solution

--- resistor-color/resistor-color.js
+++ resistor-color/resistor-color.js
@@ -3,8 +3,8 @@
 // convenience to get you started writing code faster.
 //
 
-export const colorCode = () => {
-  throw new Error("Remove this statement and implement this function");
+export const colorCode = (color) => {
+  return COLORS.indexOf(color)
 };
 
-export const COLORS = undefined;
+export const COLORS = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"];
