37c8ac91901fa713ca317abf9965fd59054892b7
Hany <hanynathan@yahoo.com>
2020-03-11 12:10:18 +0200

    final solution for resistor-color-duo exercise

--- resistor-color-duo/resistor-color-duo.js
+++ resistor-color-duo/resistor-color-duo.js
@@ -5,8 +5,15 @@
 
 const COLORS = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"];
 
-export const decodedValue = (bands) => {
-	const first = COLORS.indexOf(bands[0]);
-	const second = COLORS.indexOf(bands[1]);
-	return Number(first + '' + second);
+export const decodedValue = ([color1, color2]) => {
+	const first = COLORS.indexOf(color1);
+	const second = COLORS.indexOf(color2);
+
+	//in return using   (first * 10) + second     to avoid casting in case using Number(first + '' + second)
+	// for 3 digits case
+	// const third = 9;
+	// const digit3 = ((first * 10) + second) * 10 + third;
+	// console.log('first + second + third   ', (first * 10) + second, digit3);
+	
+	return (first * 10) + second; 
 };
